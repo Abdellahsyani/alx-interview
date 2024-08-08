b3c5b63ff63adb192d9a73e461244e7bdae476b0
Abdellahsyani <syaniabdos@gmail.com>
2024-08-08 23:43:25 +0100

    fixed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,26 +3,19 @@
 
 
 def minOperations(n):
-    '''return the number of iteration to get the n'''
+    '''return the max of operation that we do to get the n'''
     if n <= 1:
         return 0
 
     operations = 0
-    current_length = 1  # Start with one 'H'
-    clipboard = 0       # Start with an empty clipboard
+    divisor = 2
 
-    while current_length < n:
-        # Find the largest factor of the remaining length needed
-        for i in range(n // 2, 0, -1):
-            if (n - current_length) % i == 0:
-                # Copy the current length if it's a factor
-                clipboard = current_length
-                operations += 1  # Copy All operation
-                break
-
-        # Calculate how many times we need to paste to reach the next multiple
-        paste_count = (n - current_length) // clipboard
-        current_length += clipboard * paste_count
-        operations += paste_count  # Paste operations
+    # Factorize the number n
+    while n > 1:
+        while n % divisor == 0:
+            # For each factor, add the factor value to the operations
+            operations += divisor
+            n //= divisor
+        divisor += 1
 
     return operations
