a9c619a29cec25db50fe8edd7381531c12bada94
Abdellahsyani <syaniabdos@gmail.com>
2024-09-11 18:10:55 +0100

    0-rotate_2d_matrix finish

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -16,3 +16,4 @@ def rotate_2d_matrix(matrix):
             matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
             matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
             matrix[j][n - i - 1] = temp
+    print()
