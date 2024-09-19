badd65d3658f834f11f534f07499dee7f0c2d5cc
Abdellahsyani <syaniabdos@gmail.com>
2024-09-19 22:32:45 +0100

    coins done [x]

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" start changing teh coins
+"""
+
+
+def makeChange(coins, total):
+    """ the makechange method for count the total of coins
+    """
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
