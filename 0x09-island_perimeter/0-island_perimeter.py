356e0c76ff34a23c313b4e572482bfb188296009
Abdellahsyani <syaniabdos@gmail.com>
2024-09-26 10:44:38 +0100

    The perimeter island is Complete

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,12 +8,16 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if (grid[row][col] == 1):
-                if (grid[row][col - 1] == 0):
+                # check the left side of land
+                if (col == 0 or grid[row][col - 1] == 0):
                     perimeter += 1
-                if (grid[row - 1][col] == 0):
+                # check the top side of the land
+                if (row == 0 or grid[row - 1][col] == 0):
                     perimeter += 1
-                if (grid[row][col + 1] == 0):
+                # check the right side of the land
+                if (col == len(grid[0]) - 1 or grid[row][col + 1] == 0):
                     perimeter += 1
-                if (grid[row + 1][col] == 0):
+                # check the buttom side of the land
+                if (row == len(grid[0]) - 1 or grid[row + 1][col] == 0):
                     perimeter += 1
     return (perimeter)
